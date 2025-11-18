f46432c221b50b85de336df3528ce447a267324d
Svetleto Norka <b_u@abv.bg>
2025-11-18 22:20:32 +0200

    prosto reshenie

---
+++ src/ProstoReshenie.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ProstoReshenie {
+
+    private static final String DICT_URL =
+            "https://raw.githubusercontent.com/nikiiv/JavaCodingTestOne/master/scrabble-words.txt";
+
+    public static void main(String[] args) throws IOException {
+
+        // Load all words from the URL
+        List<String> dict = loadAllWords(DICT_URL);
+
+        long start = System.nanoTime();
+
+        // HashSet - > hold unique elements and have O(1) for contains()
+        HashSet<String> words = new HashSet<>();
+        HashSet<String> filteredWords = new HashSet<>();
+        HashSet<String> filteredWords9 = new HashSet<>();
+
+        words.addAll(dict);
+
+        // Filtering words that contain A or I and length<=9
+        for (String w : words) {
+            int len = w.length();
+            if (len <= 9 && (w.contains("A") || w.contains("I"))) {
+                filteredWords.add(w);
+            }
+        }
+
+        // Next filtration
+        for (String n : filteredWords) {
+            int len9 = n.length();
+            if (len9 == 9) {
+                filteredWords9.add(n);
+            }
+        }
+
+        filteredWords.removeAll(filteredWords9);
+
+
+        HashSet<String> fineFiltering = finefiltering(filteredWords9, filteredWords);
+
+
+    }
+
+    private static HashSet<String> finefiltering(HashSet<String> filteredWords9, HashSet<String> filteredWords) {
+        HashSet<String> fineFiltering = new HashSet<>();
+
+        while(!filteredWords9.isEmpty()) {
+            String word = filteredWords9.iterator().next();
+            filteredWords9.remove(word);
+
+            for (int i = 0; i < word.length(); i++) {
+
+                StringBuilder sb = new StringBuilder(word);
+                sb.deleteCharAt(i);
+                String newWord = sb.toString();
+
+                if (filteredWords.contains(newWord)) {
+                    fineFiltering.add(word);
+                    filteredWords.remove(newWord);
+                    break;
+                }
+            }
+        }
+
+        return fineFiltering;
+    }
+
+        // Load words from URL
+        private static List<String> loadAllWords (String url) throws IOException {
+            URL wordsURL = new URL(url);
+
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(wordsURL.openConnection().getInputStream()))) {
+
+                return br.lines().skip(2).collect(Collectors.toList());
+            }
+        }
+
+}
